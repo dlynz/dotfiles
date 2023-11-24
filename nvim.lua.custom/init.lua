4752420112e21ad1d6ac6c7a28889a917fa35970
dylan <robouser51@gmail.com>
2023-11-24 23:14:43 +1100

    sync

--- nvim.lua.custom/init.lua
+++ nvim.lua.custom/init.lua
@@ -1,4 +1,8 @@
-vim.opt.colorcolumn = "100"
+vim.opt.colorcolumn = "80"
 
 vim.keymap.set("n", "<C-d>", "<C-d> zz")
 vim.keymap.set("n", "<C-u>", "<C-u> zz")
+
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+
