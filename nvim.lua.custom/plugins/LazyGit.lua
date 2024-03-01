d1d39de96f472fe62abc62ce2b5393f0babd1a19
dylan <robouser51@gmail.com>
2024-03-02 03:53:11 +1100

    added nvim shortcut for lazygit

--- nvim.lua.custom/plugins/LazyGit.lua
+++ nvim.lua.custom/plugins/LazyGit.lua
@@ -8,6 +8,12 @@ local plugins = {
     		"LazyGitFilter",
     		"LazyGitFilterCurrentFile",
     	},
+        init = function ()
+            local wk = require("which-key")
+            wk.register({
+                ["<leader>lg"] = { ":LazyGit<CR>", "LazyGit" },
+            }, { mode = "n" })
+        end,
             -- optional for floating window border decoration
             dependencies = {
             "nvim-lua/plenary.nvim",
