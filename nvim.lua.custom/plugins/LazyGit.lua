e0f47123218103d936b1466bb23acfa12d263e02
dylan <robouser51@gmail.com>
2024-03-02 03:50:01 +1100

    added lazygit.nvim

---
+++ nvim.lua.custom/plugins/LazyGit.lua
@@ -0,0 +1,18 @@
+local plugins = {
+    {
+        "kdheepak/lazygit.nvim",
+    	cmd = {
+    		"LazyGit",
+    		"LazyGitConfig",
+    		"LazyGitCurrentFile",
+    		"LazyGitFilter",
+    		"LazyGitFilterCurrentFile",
+    	},
+            -- optional for floating window border decoration
+            dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+}
+
+return plugins
