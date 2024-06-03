561df9986aa7578cd92de4187f6d8c86028d800d
dylan <robouser51@gmail.com>
2024-06-04 03:16:28 +1000

    vimtex

--- nvim.lua.custom/plugins/VimTex.lua
+++ nvim.lua.custom/plugins/VimTex.lua
@@ -1,10 +1,9 @@
 local plugin = {
   "lervag/vimtex",
   lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
-    -- VimTeX configuration goes here
+    vim.g['vimtex_view_method'] = 'zathura' -- for variant without xdotool to avoid errors in wayland
   end
 }
-return plugin
 
+return plugin
