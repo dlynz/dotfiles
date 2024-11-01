3c609b244d5bbc56eb55ccad4759af2e32180eff
dylan <robouser51@gmail.com>
2024-11-01 20:17:46 +1100

    add yazi

---
+++ nvim.lua.custom/plugins/Yazi.lua
@@ -0,0 +1,21 @@
+local plugin = {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>yy",
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory" ,
+    },
+  },
+}
+
+return plugin
+
